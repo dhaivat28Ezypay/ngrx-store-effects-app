3f58739dbcfecd5fcc380461ad9336b3c1af77a2
dhaivat28Ezypay <dhaivat.parikh@ezypay.com>
2022-06-16 17:10:27 +1000

    test

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
-import { tap } from 'rxjs/operators';
-import * as fromStore from '../../store';
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs/Observable";
+import { tap } from "rxjs/operators";
+import * as fromStore from "../../store";
 
-import { Pizza } from '../../models/pizza.model';
-import { Topping } from '../../models/topping.model';
+import { Pizza } from "../../models/pizza.model";
+import { Topping } from "../../models/topping.model";
 
 @Component({
-  selector: 'product-item',
+  selector: "product-item",
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrls: ['product-item.component.scss'],
+  styleUrls: ["product-item.component.scss"],
   template: `
-    <div 
-      class="product-item">
+    <div class="product-item">
       <pizza-form
         [pizza]="pizza$ | async"
         [toppings]="toppings$ | async"
         (selected)="onSelect($event)"
         (create)="onCreate($event)"
         (update)="onUpdate($event)"
-        (remove)="onRemove($event)">
-        <pizza-display
-          [pizza]="visualise$ | async">
-        </pizza-display>
+        (remove)="onRemove($event)"
+      >
+        <pizza-display [pizza]="visualise$ | async"> </pizza-display>
       </pizza-form>
     </div>
   `,
@@ -41,9 +39,11 @@ export class ProductItemComponent implements OnInit {
       tap((pizza: Pizza = null) => {
         const pizzaExists = !!(pizza && pizza.toppings);
         const toppings = pizzaExists
-          ? pizza.toppings.map(topping => topping.id)
+          ? pizza.toppings.map((topping) => topping.id)
           : [];
         this.store.dispatch(new fromStore.VisualiseToppings(toppings));
+
+        console.log(pizza);
       })
     );
     this.toppings$ = this.store.select(fromStore.getAllToppings);
@@ -63,7 +63,7 @@ export class ProductItemComponent implements OnInit {
   }
 
   onRemove(event: Pizza) {
-    const remove = window.confirm('Are you sure?');
+    const remove = window.confirm("Are you sure?");
     if (remove) {
       this.store.dispatch(new fromStore.RemovePizza(event));
     }
